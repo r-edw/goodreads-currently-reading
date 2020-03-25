2f06681cdcb0c9995eaa6bf3f2129133517e38e8
r-edw <aaabbc1@hotmail.co.uk>
2020-03-25 21:30:36 +0000

    adding some basic caching

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const express = require('express');
 const axios = require('axios');
 const parseString = require('xml2js').parseString;
 
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache();
+
 const server = express();
 
 const GOOREADS_HOST = "https://www.goodreads.com";
+const HOURS_IN_SECONDS_12 = 43200;
 
 const returnError = (res) => {
   res.json({
@@ -15,6 +19,14 @@ const returnError = (res) => {
 }
 
 server.get("/shelf/currently_reading", (req, res) => {
+  value = myCache.get(req.url);
+  if (value !== undefined){
+    console.log("retrieved from cache");
+    res.json({ message: value });
+
+    return;
+  }
+
   axios.get(`${GOOREADS_HOST}/review/list/${process.env.GOODREADS_ID}?key=${process.env.GOODREADS_KEY}&shelf=currently-reading`)
     .then((response) => {
       parseString(response.data, (err, result) => {
@@ -26,6 +38,9 @@ server.get("/shelf/currently_reading", (req, res) => {
 
         try {
           const books = result.GoodreadsResponse.books;
+
+          myCache.set(req.url, books, HOURS_IN_SECONDS_12);
+          console.log("set in cache");
 
           res.json({ message: books });
         } catch {
